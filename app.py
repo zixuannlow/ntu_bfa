7770540629967e815daa9cc56a2d3eed58aa7152
zixuannlow <134292826+zixuannlow@users.noreply.github.com>
2024-09-21 07:01:34 +0000

    app done

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def index(): 
     return(render_template("index.html"))
 
+@app.route("/prediction_DBS", methods=["GET","POST"])
+def prediction_DBS(): 
+    return(render_template("prediction_DBS.html"))
+
+@app.route("/prediction_DBS_result", methods=["GET","POST"])
+def prediction_result_DBS(): 
+    q= float(request.form.get("q"))
+    r= -50.6*q + 90.2
+    return(render_template("prediction_DBS_result.html", r=r))
+
 if __name__ == "__main__": 
     app.run() #to change port number: app.run(port=1234)
 
